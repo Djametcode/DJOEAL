4a59735587e5ad184e2c3df1bb818e99fdd77a9f
djametcode <kampretsiji1@gmail.com>
2023-04-07 20:32:30 +0700

    changes

--- frontend/DJUAL.ID/src/component/menu.jsx
+++ frontend/DJUAL.ID/src/component/menu.jsx
@@ -40,7 +40,7 @@ const Menu = () => {
           autoPlay
           muted
           loop
-          src="./video/degraded-119282.mp4"
+          src="./src/video/degraded-119282.mp4"
         ></video>
         {/* <div className=" font-alata text-white/70 p-3 text-base">
           <p>Looking For Something?</p>
